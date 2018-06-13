f356dbc3bc44eb9e83d4f03d8071b714d1885700
ChenTao <bz20100405@163.com>
2018-06-14 01:49:54 +0800

    update DB structure
    already update Login getUserInfo Logout Register api
    todo: updateInfo updatePassword

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'ball',
+  database: 'BallHub',
   insecureAuth : true,
 });
 
@@ -27,80 +27,90 @@ app.get(/\//, function (req, res) {
 
 app.post(/\//, function (req, res) {
     console.log("POST "+req.path);
+    console.log(req.body);
 ////////////////////////////////////////////////////////////////////////////
-//#login    
-    if(req.path == "/api/login") {
-        connection.query('SELECT * FROM user where username="'+req.body.username+'" and password="'+req.body.password+'"', function(err, rows, fields) {
-            if(rows.length == 0) {
-                console.log("error password");
-                res.status(401).send('ERROR PASSWORD');
-                return;
-            } else {
-                var role = rows[0].role;
-                console.log(role);
-                connection.query('SELECT * from user,?? where user.username=? and ??.username=?',
-                [role, req.body.username, role, req.body.username],
-                function(err, rows, fields) {
-                    console.log(rows);
-                    res.send(rows[0]);                    
-                });
-            }
-        });
-    }
-////////////////////////////////////////////////////////////////////////////
-//#getUserInfo     
+//#login   正常登录 
+//#getUserInfo   通过Cookies登录
+  if(req.path == "/api/login" || req.path == "/api/getUserInfo") {
+    var data;
     if(req.path == "/api/getUserInfo") {
-        console.log(req.body);                  //用于处理cookies登录
-        if(JSON.stringify(req.body) == '{}' || req.body.cookies == '') {
-            res.status(401).send('ERROR');
-            return; 
-        } else {
-            var data = JSON.parse(req.body.cookies);
-            connection.query('SELECT * FROM user where username="'+data.username+'" and password="'+data.password+'"', function(err, rows, fields) {
-                if(rows.length == 0) {
-                    console.log("error");
-                    res.status(401).send('ERROR');
-                    return;                     
-                } else {
-                    console.log("ok");
-                    var role = rows[0].role;
-                    connection.query('SELECT * from user,?? where user.username=? and ??.username=?',
-                    [role, data.username, role, data.username],
-                    function(err, rows, fields) {
-                        console.log(rows);
-                        res.status(200).send(rows[0]);                    
-                    });
-                }
-            });            
-        }
+      if(JSON.stringify(req.body) == '{}' || req.body.cookies == '') {
+        res.status(401).send('ERROR');
+        return; 
+      } else {
+        data = JSON.parse(req.body.cookies);
+      }
+    } else {
+      data = req.body;
     }
+    console.log(data);
+    //确认用户名和密码正确
+    connection.query('SELECT * FROM user where username=? and password=?',
+    [data.username, data.password],
+    function(err, rows, fields) {
+      if(rows.length == 0) {
+        console.log("error password");
+        res.status(401).send('ERROR PASSWORD');
+        return;
+      } else {
+        console.log(rows[0]);
+        var id = rows[0].uid;
+        //在ISA关系中查找该用户的角色
+        connection.query('SELECT role from ISA where uid=?',
+        [id],
+        function(err, rows, fields) {
+          //通过用户id从user表和对应角色(player/referee/team)表中获得用户信息
+          connection.query('SELECT * from user,ISA,?? where user.uid=? and ISA.uid=? and ??.uid=?',
+          [rows[0].role, id, id, rows[0].role, id],
+          function(err, rows, fields) {
+            console.log(rows);
+            res.status(200).send(rows[0]);
+          });                    
+        });
+      }
+    });
+  }
 ////////////////////////////////////////////////////////////////////////////
 //#logout      
-    if(req.path == "/api/logout") {
-        res.status(200).send('Logout success');
-        return;
-    }
+  if(req.path == "/api/logout") {
+    res.status(200).send('Logout success');
+    return;
+  }
 ////////////////////////////////////////////////////////////////////////////
 //#register   
-    if(req.path == "/api/register") {
-        connection.query('SELECT * FROM user where username="'+req.body.username+'"', function(err, rows, fields) {
-            if(rows.length == 0) {
-                connection.query('INSERT into user (username, password, role) values (?,?,?)',
-                [req.body.username, req.body.password, req.body.role],function(err, rows, fields) {
-                    connection.query('INSERT into ?? (username) values (?)',
-                    [req.body.role, req.body.username],function(err, rows, fields) {
-                        if(err)
-                            console.log(err);                        
-                        res.status(200).send('OK');                  
-                    });                                    
-                });
-            } else {
-                console.log("username has been used!");
-                res.status(401).send('USERNAME HAS BEEN USED');
-                return;
-            }
-        });
-    }
+  if(req.path == "/api/register") {
+    connection.query('SELECT * FROM user where username=?',
+    [req.body.username],
+    function(err, rows, fields) {
+      if(rows.length == 0) {
+          connection.query('INSERT into user (username, password) values (?,?)',
+          [req.body.username, req.body.password],
+          function(err, rows, fields) {
+            //获取自动生成的pid(自增的一个整数)
+            connection.query('SELECT uid from user where username=?',
+            [req.body.username],
+            function(err,rows,fields) {
+              var id = rows[0].uid
+              //将用户id加入ISA关系表
+              connection.query('INSERT into ISA(uid,role) values (?,?)',
+              [id, req.body.role],
+              function(err,rows,fields) {
+                //将用户id加入具体角色信息表
+                connection.query('INSERT into ?? (uid) values (?)',
+                [req.body.role, id],
+                function(err,rows,fields) {
+                  res.status(200).send('OK'); 
+                });  
+              });            
+            });
+          });
+      } else {
+          console.log("username has been used!");
+          res.send('USERNAME HAS BEEN USED');
+          return;
+      }
+    });
+  }
 ////////////////////////////////////////////////////////////////////////////
 //#updateLoginInfo
     if(req.path == "/api/updateLoginInfo") {
